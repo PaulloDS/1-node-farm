3c9b7173eb9d41d95cf90340aed8a5b7c62a4b63
PaulloDS <paulosch14@gmail.com>
2024-04-19 01:18:27 -0300

    feat: att ports

--- index.js
+++ index.js
@@ -76,7 +76,8 @@ const server = http.createServer((req, res) => {
     };
 });
 
-server.listen(8000, 'localhost', () => {
+server.listen(8000, '0.0.0.0', () => {
     console.log("Listening on port 8000");
 });
+
 
